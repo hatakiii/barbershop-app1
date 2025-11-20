886f0999dcf68e4cd6240aeacdc30bb400f2afb4
Popeyeez <lozo022011@gmail.com>
2025-11-20 11:02:40 +0800

    booking page uusgev

---
+++ app/booking/page.tsx
@@ -0,0 +1,10 @@
+import { Calendar } from "@/components/ui/calendar";
+
+export default function BookingPage() {
+  return (
+    <div>
+      <h1>Цаг захиалах</h1>
+      <Calendar></Calendar>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,14 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function Home() {
-  return <div>Hehehahaha</div>;
+  return (
+    <div className="w-200 h-200 flex justify-center items-center">
+      <Link href="/booking">
+        <button className="px-4 py-2 bg-blue-600 text-white rounded">
+          Book
+        </button>
+      </Link>
+    </div>
+  );
 }
