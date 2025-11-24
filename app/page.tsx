725d87d34a48b17afadc333827fe119081e200d8
Khatanbaatar A <25lp5598@nest.edu.mn>
2025-11-24 09:40:09 +0800

    Luxe Hair Salon Frontend nuur huudsan deer nemlee

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,23 @@
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
+"use client";
+import { Header } from "./_components/Header";
+import { Hero } from "./_components/Hero";
+import { Services } from "./_components/Services";
+import { Team } from "./_components/Team";
+import { Gallery } from "./_components/Gallery";
+import { Contact } from "./_components/Contact";
+import { Footer } from "./_components/Footer";
 
 export default function Home() {
   return (
-    <div className="w-200 h-200 flex justify-center items-center">
-      <Link href="/booking">
-        <button className="px-4 py-2 bg-blue-600 text-white rounded">
-          Book
-        </button>
-      </Link>
+    <div>
+      Barber Shop Frontend
+      <Header />
+      <Hero />
+      <Services />
+      <Team />
+      <Gallery />
+      <Contact />
+      <Footer />
     </div>
   );
 }
