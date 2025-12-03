f3c28ae20a1b94a4b094c08fe237970882627961
Khatanbaatar A <25lp5598@nest.edu.mn>
2025-12-03 12:31:14 +0800

    mapnii aldaag zaslaa

--- app/_components/Map.tsx
+++ app/_components/Map.tsx
@@ -42,9 +42,12 @@ export default function Map({
 
       // fix icon
       const DefaultIcon = L.icon({
-        iconRetinaUrl,
-        iconUrl,
-        shadowUrl,
+        iconRetinaUrl:
+          "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png",
+        iconUrl:
+          "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png",
+        shadowUrl:
+          "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png",
         iconSize: [25, 41],
         iconAnchor: [12, 41],
       });
