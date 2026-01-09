e837f7ce716c692d3fc05f231a7b605f71d3ffdb
Khatanbaatar A <hbbaatar@gmail.com>
2026-01-09 11:20:03 +0800

    header style

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -19,7 +19,7 @@ export function Header() {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <header className="fixed top-0 left-0 right-0 z-50  backdrop-blur-sm border-b ">
+    <header className="fixed top-0 left-0 right-0 z-50  backdrop-blur-sm border-b bg-pink-300 ">
       <div className="max-w-6xl mx-auto px-6">
         <div className="flex items-center justify-between h-16">
           {/* Logo */}
